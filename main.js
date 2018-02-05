899db880d99e30bfec12b68ad4a732a8135eee28
Marco Blum <marco8jnv@hotmail.com>
2018-02-05 15:05:35 +0100

    fixed helge misunderstanding

--- main.js
+++ main.js
@@ -1,32 +1,59 @@
 const fs = require('fs');
-var db = "data.txt";
-var map = new Map()
+let db = "data.txt";
+let database = "database.txt";
+let map = new Map()
 
-function db_write(key, value) {
-    map[key] = value;
+function db_write(key, data) {
+    map[key] = fs.statSync("database.txt").size;
+    fs.appendFile(database, key + "," + data + "\n", err => {
+        if (err) { throw err }
+    })
     return "saved at: " + key
 }
 
-function db_read(key) {
-    if (map.hasOwnProperty(key)) { return { key: key, value: map[key] }; }
-    //throw new Error("invalid map key: "+key);
-    return "invalid map key: " + key
+function db_read(key, callback) {
+    if (map.hasOwnProperty(key)) {
+        fs.open(database, 'r', function (status, fd) {
+            let next = Object.keys(map).indexOf(key) + 1;
+            if (next >= Object.keys(map).length) {
+                next = fs.statSync("database.txt").size - map[key];
+            } else {
+                next = map[Object.keys(map)[next]] - map[key];
+            }
+            let buffer = new Buffer(next);
+            fs.read(fd, buffer, 0, next, 0, function () {
+                callback({ key: key, offset: map[key], data: buffer.toString('utf8').slice(0, -1) });
+            });
+        });
+    }else{callback("invalid map key: " + key)}
 }
 
-function db_read_index(index) {
-
-    var key = Object.keys(map)[index];
+function db_read_index(index, callback) {
+    let key = Object.keys(map)[index];
     if (key == undefined) {
-        //throw new Error("invalid index: "+index);
-        return "invalid index: " + index;
+        callback("invalid index: " + index);
+    } else {
+        fs.open(database, 'r', function (status, fd) {
+            let next = index + 1;
+            if (next >= Object.keys(map).length) {
+                next = fs.statSync("database.txt").size - map[key];
+            } else {
+                next = map[Object.keys(map)[next]] - map[key];
+            }
+            console.log(Object.keys(map)[next], map[key])
+            let buffer = new Buffer(next);
+            fs.read(fd, buffer, 0, next, 0, function () {
+                console.log(map[key], Object.keys(map).indexOf(key), next)
+                callback({ key: key, offset: map[key], data: buffer.toString('utf8').slice(0, -1) });
+            });
+        });
     }
-    return { key: key, value: map[key] }
 }
 
 function db_save_binary() {
-    var output = "";
-    var input = JSON.stringify(map);
-    for (var i = 0; i < input.length; i++) {
+    let output = "";
+    let input = JSON.stringify(map);
+    for (let i = 0; i < input.length; i++) {
         output += input[i].charCodeAt(0).toString(2);
         if (i != input.length - 1) {
             output += " "
@@ -39,17 +66,17 @@ function db_save_binary() {
 }
 
 function db_import_binary() {
-    var temp;
+    let temp;
     binaryArr = fs.readFileSync(db, "utf8").toString().split(" ");
-    var binaryCode = [];
-    var output = "";
+    let binaryCode = [];
+    let output = "";
     for (i = 0; i < binaryArr.length; i++) {
         output += String.fromCharCode(parseInt(binaryArr[i], 2));
 
     }
     if (binaryArr.length > 1) {
         map = new Map()
-        var temp = JSON.parse(output)
+        let temp = JSON.parse(output)
         Object.keys(temp).forEach(function (k) {
             map[k] = temp[k]
         });
@@ -57,15 +84,22 @@ function db_import_binary() {
     return output
 }
 
-
-var new_key = "key" + Math.floor(Math.random() * 1000)
-
-console.log("current map:    ", JSON.stringify(map));
-console.log("imported map:   ", db_import_binary());
-console.log("save data:      ", db_write(new_key, "lots of data"));
-console.log("save data:      ", db_write(new_key + "2", "lots of other data"));
-console.log("read by key:    ", db_read(new_key));
-console.log("read by key:    ", db_read(new_key + "3"));
-console.log("read by index:  ", db_read_index(1));
-console.log("read by index:  ", db_read_index(map.size + 5));
-console.log("save to binary: ", db_save_binary());
+let new_key = "key" + Math.floor(Math.random() * 1000)
+console.log("imported map:   ",db_import_binary() ,"\n------------------------------------------------------------");
+console.log("save data:      ", db_write(new_key, '{"stuff":"lots of data"}'));
+console.log("save data:      ", db_write(new_key + "2", '{"stuff":"lots of other data"}'));
+console.log("current map:    ", JSON.stringify(map),"\n------------------------------------------------------------");
+db_read(new_key + 3, (data) => {
+    console.log("read wrong key name:    ", data);
+})
+db_read_index(Object.keys(map).length + 2, (data) => {
+    console.log("read wrong key index:    ", data);
+    
+})
+db_read(new_key, (data) => {
+    console.log("read correct key name:    ", data);
+})
+db_read_index(0, (data) => {
+    console.log("read correct key index:    ", data,"\n------------------------------------------------------------");
+    console.log("save to binary: ", db_save_binary());
+})
